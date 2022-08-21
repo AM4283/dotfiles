102f948755dbf73ec20007ace46ca384deb82a1c
Albi <albimarini4283@gmail.com>
2022-08-20 22:55:48 -0400

    cleaned up home dir using environment variables+shell aliases, added support for ~/.config/gtk-2.0/gtkrc instead of ~/.gtkrc-2.0 in themes.sh

--- themes/themes.sh
+++ themes/themes.sh
@@ -8,18 +8,34 @@ done
 themes+=(quit)
 length=${#themes[@]}
 choice=$(printf '%s\n' "${themes[@]}" | rofi -dmenu -i -l $length -p 'Change theme')
+
+gtk2notinhome=false
+if [[ -n $GTK2_RC_FILES ]]; then
+  gtk2notinhome=true
+  # echo "true"
+else
+  gtk2notinhome=false
+  # echo "false"
+fi
+
 if [[ "$choice" == "quit" ]]; then
   echo "Program terminated." && exit 0
 elif [ "$choice" ]; then
   cp ~/.Xresources ~/themes/.Xresources.bak
-  cp ~/.gtkrc-2.0 ~/themes/.gtkrc-2.0.bak
   cp ~/.config/gtk-3.0/settings.ini ~/themes/settings.ini.bak 
   cp $choice/.Xresources ~/
-  cp $choice/.gtkrc-2.0 ~/
+  if [ $gtk2notinhome == true ]; then
+    cp $GTK2_RC_FILES ~/themes/gtkrc.bak
+    cp $choice/.gtkrc-2.0 $GTK2_RC_FILES
+  else
+    cp ~/.gtkrc-2.0 ~/themes/.gtkrc-2.0.bak
+    cp $choice/.gtkrc-2.0 ~/
+  fi
   cp $choice/settings.ini ~/.config/gtk-3.0/
   cursor="$(grep "gtk-cursor-theme-name" $choice/settings.ini | awk -F = '{print $2}')"
   icon_theme="$(grep "gtk-icon-theme-name" $choice/settings.ini | awk -F = '{print $2}')"
   sed -i "s/Inherits=.*/Inherits=${cursor}/" ~/.icons/default/index.theme
+  sed -i "s/Inherits=.*/Inherits=${cursor}/" ~/.local/share/icons/default/index.theme
   xrdb ~/.Xresources
   ./rofi.sh 1 > ~/.config/rofi/themes/colors.rasi
   ./rofi.sh 2 $icon_theme > ~/.config/rofi/config.rasi
